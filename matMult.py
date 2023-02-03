fac6d9c2307ea50ef56ca27470e4245f5068b329
Trevor Chan <trevchan@bu.edu>
2023-02-02 23:36:52 -0500

    Checked code with flake8 and added flake8 to Github Actions

--- matMult.py
+++ matMult.py
@@ -1,54 +1,58 @@
-import flake8
-import pytest
 import tracemalloc
-import numpy as np
 import logging
 import cProfile
-import re
 
 
+# Flake8 used in Github Actions
 def matMult(mat1, mat2):
-    tracemalloc.start()
-    logging.basicConfig(filename='matMult.log', filemode='w', format='%(asctime)s %(levelname)s - %(message)s')
 
-    logging.info('input 1: %d',mat1)
+    tracemalloc.start()
+
+    # Set logging to log any errors/warnings to matMult.log
+    logging.basicConfig(filename='matMult.log',
+                        filemode='w',
+                        format='%(asctime)s %(levelname)s - %(message)s')
+
+    # Log basic info about inputs
+    logging.info('input 1: %d', mat1)
     logging.info('input 2: %d', mat2)
-    #Error code -1: Checking if mat1 input is empty
-    if( mat1 == None ):
+
+    # Error -1: Checking if mat1 input is empty
+    if (mat1 is None):
         print('Error: Matrix 1 is empty')
         logging.error('Matrix 1 is empty')
         return -1
 
-    #Error code -2: Checking if mat2 input is empty
-    if( mat2 == None ):
+    # Error -2: Checking if mat2 input is empty
+    if (mat2 is None):
         print('Error: Matrix 2 is empty')
         logging.error("Matrix 2 is empty")
         return -2
-    
-    #Error code -3: Checking if the input datatypes are valid (both lists)
-    if( type(mat1) != list or type(mat2) != list):
+
+    # Error -3: Checking if the input datatypes are valid (both lists)
+    if (type(mat1) != list or type(mat2) != list):
         print("Error: Invalid input datatype")
         logging.error("Invalid input datatype")
         return -3
 
-    #Error code -4: Checking if mismatch in lengths of rows / columns in mat1
+    # Error -4: Checking if mismatch in lengths of rows / columns in mat1
     for row in mat1:
-        if(len(row) != len(mat1[0])):
-            print("Error: Not a valid Matrix 1 (Rows / Columns of varying sizes")
-            logging.error("Error: Not a valid Matrix 1 (Rows / Columns of varying sizes")
+        if (len(row) != len(mat1[0])):
+            print("Error: Input 1 has varying row sizes")
+            logging.error("Error: Input 1 has varying row sizes")
             return -4
 
-    #Error code -5: Checking if mismatch in lengths of rows / columns in mat2
+    # Error -5: Checking if mismatch in lengths of rows / columns in mat2
     for row in mat2:
-        if(len(row) != len(mat2[0])):
-            print("Error: Not a valid Matrix 2 (Rows / Columns of varying sizes")
-            logging.error("Error: Not a valid Matrix 2 (Rows / Columns of varying sizes")
+        if (len(row) != len(mat2[0])):
+            print("Error: Input 2 has varying row sizes")
+            logging.error("Error: Input 2 has varying row sizes")
             return -5
-    
-    #Error code -6: Checking if the sizes work (if the # columns in mat1 = # rows in mat2)
-    if( len(mat1[0]) != len(mat2) ):
-        logging.error("Error: Invalid Matrix Size Mismatch (# columns of Matrix 1 must equal # rows of matrix 2")
-        print("Error: Invalid Matrix Size Mismatch (# columns of Matrix 1 must equal # rows of matrix 2")
+
+    # Error -6: Checking if the # columns in mat1 = # rows in mat2
+    if (len(mat1[0]) != len(mat2)):
+        logging.error("Error: Matrix Size Mismatch")
+        print("Error: Matrix Size Mismatch")
         return -6
 
     else:
@@ -61,6 +65,7 @@ def matMult(mat1, mat2):
                     for k in range(len(mat2)):
                         num += mat1[i][k] * mat2[k][j]
                     result[i][j] = num
+
             logging.debug("Successfully ran Matrix Multiplication")
             cProfile.run('re.compile("foo|bar")')
             snapshot = tracemalloc.take_snapshot()
@@ -71,9 +76,8 @@ def matMult(mat1, mat2):
             return result
         except Exception as e:
             logging.error("Exception occured", exc_info=True)
-        
-    
-            
+            print("Exception %s", e)
+
 
 mat1 = [
     [1, 2, 3],
@@ -86,5 +90,4 @@ mat2 = [
     [7, 8, 9]
 ]
 
-print(matMult(mat1, mat2))
-    
+print(matMult(None, mat2))
